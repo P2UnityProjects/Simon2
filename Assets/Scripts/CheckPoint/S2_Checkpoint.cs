318a519148d750d18046f9ffe0041e706ce51404
Nicolas DEROSES <nicolas2roz@gmail.com>
2021-12-02 14:17:20 +0100

    Checkpoint

--- Assets/Scripts/CheckPoint/S2_Checkpoint.cs
+++ Assets/Scripts/CheckPoint/S2_Checkpoint.cs
@@ -8,15 +8,24 @@ public class S2_Checkpoint : MonoBehaviour
 
     public bool IsValid => checkpointCollider;
 
+    public Vector3 CheckPointPosition => transform.position;
+
     #endregion
 
     #region Methods
+
+    public void UseCheckPoint()
+    {
+        //LastCameraOffset
+    }
+    
+
 
     private void OnTriggerEnter(Collider other)
     {
         S2_Player _player = other.GetComponent<S2_Player>();
         if (!_player) return;
-
+        _player.SetCheckpoint(this);
     }
 
     #endregion
