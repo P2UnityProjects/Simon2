fd2e18de6fb86ea2dea1809067abdec8d1f41734
Tominus <Tominusstarwars@gmail.com>
2021-12-03 10:32:10 +0100

    5

--- Assets/Character/Scripts/S2_CharacterCounter.cs
+++ Assets/Character/Scripts/S2_CharacterCounter.cs
@@ -1,54 +1,61 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class S2_CharacterCounter : MonoBehaviour
 {
     public event Action OnAttack = null;
 
-    [SerializeField] LayerMask toCounterLayer = 0;
+    [SerializeField] S2_Player player = null;
     [SerializeField] float shieldRadius = 5;
+    [SerializeField] LayerMask toCounterLayer = 0;
+
+    public bool IsValid => player;
+    public Vector3 Position => transform.position;
 
     private void Start()
     {
-        S2_InputManager.Instance.BindAction(S2_ButtonEvent.SHIELD, DetectDangerGPE);
+        Init();
     }
-    void DetectDangerGPE(bool _bool)
-    {
-        OnAttack?.Invoke();
-        //bool _hit = false;
-        if (_bool)
-        {
-       
-            //_hit = Physics.SphereCast(transform.position, shieldRadius, transform.forward, out RaycastHit _hitInfo, 0.1f, toCounterLayer);
-            Collider[] touchtab = Physics.OverlapSphere(transform.position, shieldRadius, toCounterLayer);
-            if (touchtab.Length == 0) return;
-            for (int i = 0; i < touchtab.Length; i++)
-            {
-         
-                // if (!_hit) return;
-                GameObject _gameO = touchtab[i].transform.gameObject;
-                if (!_gameO) return;
-                  S2_GPERocket _proj = _gameO.GetComponent<S2_GPERocket>();
-                if (!_proj) return;
-                _proj.SetTarget(_proj.GetLauncher().transform);
-                _proj.SetIsCountered(true);
-
-           
-
-            }
-        }
-    }
-
-    private void OnDrawGizmos()
-    {
-        Gizmos.DrawWireSphere(transform.position, shieldRadius);
-    }
-
     private void OnDestroy()
     {
         S2_InputManager.Instance.UnBindAction(S2_ButtonEvent.SHIELD, DetectDangerGPE);
     }
+    private void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(Position, shieldRadius);
+    }
 
+    void Init()
+    {
+        S2_InputManager.Instance.BindAction(S2_ButtonEvent.SHIELD, DetectDangerGPE);
+        player = GetComponent<S2_Player>();
+    }
+    void DetectDangerGPE(bool _bool)
+    {
+        OnAttack?.Invoke();
+        if (!_bool) return;
+
+        Collider[] touchtab = Physics.OverlapSphere(Position, shieldRadius, toCounterLayer);
+        if (touchtab.Length == 0) return;
+        for (int i = 0; i < touchtab.Length; i++)
+        {
+            Collider _collider = touchtab[i];
+            if (!_collider) continue;
+            DetectRocket(_collider);
+            DetectCollectibleBox(_collider);
+        }
+    }
+    void DetectRocket(Collider _other)
+    {
+        S2_GPERocket _proj = _other.GetComponent<S2_GPERocket>();
+        if (!_proj) return;
+        _proj.SetTarget(_proj.GetLauncher().transform);
+        _proj.SetIsCountered(true);
+    }
+    void DetectCollectibleBox(Collider _other)
+    {
+        S2_CollectibleBox _collectibleBox = _other.GetComponent<S2_CollectibleBox>();
+        if (!_collectibleBox || !IsValid) return;
+        _collectibleBox.DestroyBox(player);
+    }
 }
